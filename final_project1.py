b46a2a2e44bc7c2631cc73028e0360222d155e57
Nijaid <jnijaid.arre@berkeley.edu>
2016-04-06 19:51:07 -0700

    -a

--- final_project1.py
+++ final_project1.py
@@ -1,6 +1,7 @@
 from numpy import *
 from random import random
 from mayavi import mlab
+G=6.67e-11
 
 class body(object):
 	def __init__(self, mass, px, py, pz, vx, vy, vz, nm):
@@ -16,8 +17,8 @@ class body(object):
 		distance_x = other.position[0] - self.position[0]
 		distance_y = other.position[1] - self.position[1]
 		distance_z = other.position[2] - self.position[2]
-		distance = (distance_x**2 + distance_y**2 + distance_z**2)**(1/2.)
-		force = G * self.mass * other.mass / (distance**2)
+		distance = (distance_x**2. + distance_y**2. + distance_z**2.)**(1./2.)
+		force = G * self.mass * other.mass / (distance**2.)
 		force_x = force * (distance_x / distance)
 		force_y = force * (distance_y / distance)
 		force_z = force * (distance_z / distance)
@@ -27,43 +28,49 @@ def interact(bodies, timestep, step_num):
 	forces_x = []
 	forces_y = []
 	forces_z = []
-	for i in bodies:
-		#find force
-		total_fx = 0
-		total_fy = 0
-		total_fz = 0
-		for j in bodies:
-			#find the other body
-			if i != j:
-				#check if the two are different
-				forces = i.gravity(j)
-				total_fx += forces[0]
-				#total force in x for each body
-				total_fy += forces[1]
-				#total force in y for each body
-				total_fz += forces[2]
-		forces_x.append(total_fx)
-		#append force in x for each body
-		forces_y.append(total_fy)
-		#append force in y for each body
-		forces_z.append(total_fz)
-	for k in range(len(forces_x)):
-		acc_x = forces_x[k] / bodies[k].mass
-		acc_y = forces_y[k] / bodies[k].mass
-		acc_z = forces_z[k] / bodies[k].mass
-		bodies[k].velocity[0] += acc_x * timestep
-		bodies[k].velocity[1] += acc_y * timestep
-		bodies[k].velocity[2] += acc_z * timestep
-		#update velocity
-		bodies[k].position[0] += bodies[k].velocity[0] * timestep
-		bodies[k].position[1] += bodies[k].velocity[1] * timestep
-		bodies[k].position[2] += bodies[k].velocity[2] * timestep
-		#update position
-	return bodies
+	step = 1
+	while step <= step_num:
+		for i in bodies:
+			#find force
+			total_fx = 0
+			total_fy = 0
+			total_fz = 0
+			for j in bodies:
+				#find the other body
+				if i != j:
+					#check if the two are different
+					forces = i.gravity(j)
+					total_fx += forces[0]
+					#total force in x for each body
+					total_fy += forces[1]
+					#total force in y for each body
+					total_fz += forces[2]
+			forces_x.append(total_fx)
+			#append force in x for each body
+			forces_y.append(total_fy)
+			#append force in y for each body
+			forces_z.append(total_fz)
+		for k in range(len(forces_x)):
+			acc_x = forces_x[k] / bodies[k].mass
+			acc_y = forces_y[k] / bodies[k].mass
+			acc_z = forces_z[k] / bodies[k].mass
+			bodies[k].velocity[0] += acc_x * timestep
+			bodies[k].velocity[1] += acc_y * timestep
+			bodies[k].velocity[2] += acc_z * timestep
+			#update velocity
+			bodies[k].position[0] += bodies[k].velocity[0] * timestep
+			bodies[k].position[1] += bodies[k].velocity[1] * timestep
+			bodies[k].position[2] += bodies[k].velocity[2] * timestep
+			#update position
+			bodies[k].xpos.append(bodies[k].position[0])
+			bodies[k].ypos.append(bodies[k].position[1])
+			bodies[k].zpos.append(bodies[k].position[2])
+		return bodies
+		step += 1
 
 def plotting(bodies):
 	for b in bodies:
-		mlab.points3d(b.xpos, b.ypos, b.zpos)
+		mlab.points3d(b.xpos, b.ypos, b.zpos, scale_factor=10)
 		#points3d takes a fourth input (a number) that can determine the size/color of the point
 
 def randomize(num_bodies, init_radius, init_max_mass, init_max_v):
@@ -90,20 +97,20 @@ def randomize(num_bodies, init_radius, init_max_mass, init_max_v):
 def simulate():
 	step_num = 0
 	previous_bodies = initialize(input1)
-	while pause = False:
-		mlab.clf()
-		interact(previous_bodies, timestep, step_num)
-		plotting()
-		previous_bodies = bodies
-		step_num += 1
+	#while pause = False:
+#		mlab.clf()
+#		interact(previous_bodies, timestep, step_num)
+#		plotting()
+#		previous_bodies = bodies
+#		step_num += 1
 
-def merge(bodies):
-	for i in bodies:
-		for j in bodies:
-			if 
+#def merge(bodies):
+#	for i in bodies:
+#		for j in bodies:
+#			if 
 
-def pause():
-	pass
+#def pause():
+#	pass
 
 #################################
 #Solar System
